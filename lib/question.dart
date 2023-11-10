52e36a9ae0c992add4875e5205e76e99d97ab6d5
gino-lr <gino.liza@ucsp.edu.pe>
2023-11-10 10:51:07 -0500

    incompleto... continuar

--- lib/question.dart
+++ lib/question.dart
@@ -2,7 +2,7 @@ class Question {
   String questionText = '';
   bool questionAnswer = true;
 
-  Question({String q = '', bool a = true}) {
+  Question(String q, bool a) {
     questionText = q;
     questionAnswer = a;
   }
